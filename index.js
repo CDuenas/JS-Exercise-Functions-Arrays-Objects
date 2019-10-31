a7afd591ebd7a8fe538d51890ea0624ed7ec1470
Christian <Chrisaduenas@gmail.com>
2019-10-30 19:04:09 -0700

    challenge 7

--- index.js
+++ index.js
@@ -140,9 +140,12 @@ function getLastCarInfo(array) {
  * For example, if getCarInfoById is invoked with the inventory and the number 1,
  * it will return `This is a Lincoln Navigator`.
 */
-function getCarInfoById(array, number) {
-  number = array.id;
-  return 'This is a ' + array.car_make + '' + array.car_model;
+function getCarInfoById(array, id) {
+  for (let i = 0; i < array.length; i++){
+    if (array[i].id === id){
+      return 'This is a ' + array[i].car_make + array[i].car_model;
+    }
+  }
 }
 
 /**
