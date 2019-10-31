8b17c01675b1b836e32e0b4e780f145b95120ee4
CDuenas <chrisaduenas@gmail.com>
2019-10-31 01:17:32 -0700

    modelYears func

--- index.js
+++ index.js
@@ -156,17 +156,18 @@ function getCarInfoById(array, id) {
  *     (1) an array which is an inventory of cars like the one inside /data/inventory.js.
  * sortCarInventory returns an inventory that is sorted by car_model, ascending [A-Z].
 */
-function sortCarInventory(array) {
+function sortCarInventory(array) {  
   return array.sort(function(a, b){
     if (a.car_model < b.car_model) {
       return -1;
-    } 
+    }
     if (a.car_model > b.car_model){
       return 1;
     }
-
     return 0;
+  })
 }
+
 
 /**
  * ### Challenge `getModelYears`
@@ -177,9 +178,15 @@ function sortCarInventory(array) {
  *     (1) an array which is an inventory of cars like the one inside /data/inventory.js.
  * getModelYears returns an array containing all the 'car_year's in the inventory.
 */
-function getModelYears(/* code here */) {
-  /* code here */
-}
+function getModelYears(array) {
+  let modelYears = [];
+  for (let i = 0; i < array.length; i++){
+      modelYears.push(array[i].car_year)
+    }
+    return modelYears;
+  }
+  
+
 
 /**
  * ### Challenge `getOlderCars`
