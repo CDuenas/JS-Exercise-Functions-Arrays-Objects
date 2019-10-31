206d9551be9e393ba4382a7c23e935e353a95e5f
CDuenas <chrisaduenas@gmail.com>
2019-10-31 01:45:19 -0700

    Oldercars

--- index.js
+++ index.js
@@ -200,8 +200,14 @@ function getModelYears(array) {
  * with a `car_year` which is at most the given desired max year,
  * in the same order as they appear in the original inventory.
 */
-function getOlderCars(/* code here */) {
-  /* code here */
+function getOlderCars(arr, maxYear) {
+  const oldCars = [];
+  for (let i = 0; i < arr.length; i++){
+    if (arr[i].car_year <= maxYear){
+      oldCars.push(arr[i])
+    }
+  }
+  return oldCars;
 }
 
 /**
